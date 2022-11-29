c3cbfda09112330d0c14d3f32274e626f19255a9
adchella-te <adchella@thousandeyes.com>
2022-11-29 13:37:12 +0100

    updated styleguide js  and added functions tasks

---
+++ static/js/task.js
@@ -0,0 +1,9 @@
+function taskson(response) {
+    document.getElementById("test").innerHTML = "Hello World";
+    console.log('Tasks On', response);
+}
+
+function tasksoff(response) {
+    document.getElementById("test").innerHTML = "Hello World";
+    console.log('Tasks Off', response);
+}
