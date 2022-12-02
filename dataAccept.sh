62ee0a9220d1796b7f3eb12cec52caec5d6f1639
Soprek <tsoprek@gmai.com>
2022-12-02 19:15:51 +0100

    scripts Drop/Accept for IP tables run into issue due to ip address change. Explanation: IP ADDRESSES OF THE registry.agt.thousandeyes.com change from time to time. Previous script did dig and stored output to address variable.
    When this change is done while TSing or machine was powered off and address is changed, above script will fail to remove iptables rules as it is not in table - it is doing dig and get new IP address! Hence, new script that will store ip addresses to a file, read from a file. I will add more improvements to check if rule exist before trying to remove.

--- dataAccept.sh
+++ dataAccept.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-ipaddr=`dig +short data1.agt.thousandeyes.com |grep -v .com`
-echo $ipaddr | sed 's/ /\n/' | while read line; 
-  do
-  echo $line
-  iptables -D OUTPUT -d $line -p tcp --dport 443 -j DROP
-  done
+#ipaddr=`dig +short data1.agt.thousandeyes.com |grep -v .com`
+#echo $ipaddr | sed 's/ /\n/' | while read line;
+#  do
+#  echo $line
+#  iptables -D OUTPUT -d $line -p tcp --dport 443 -j DROP
+#  done
 
-
+sort -u known_data_ip | while read address;
+do
+iptables -D OUTPUT -d $addresses -p tcp --dport 443 -j DROP
+done
--- dataDrop.sh
+++ dataDrop.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-ipaddr=`dig +short data1.agt.thousandeyes.com |grep -v .com`
-addresses=`echo $ipaddr | sed 's/ /,/'`
-echo $addresses
-iptables -A OUTPUT -d $addresses -p tcp --dport 443 -j DROP 2>&1
+#ipaddr=`dig +short data1.agt.thousandeyes.com |grep -v .com`
+#addresses=`echo $ipaddr | sed 's/ /,/'`
+#echo $addresses
+#iptables -A OUTPUT -d $addresses -p tcp --dport 443 -j DROP 2>&1
+
+dig +short data1.agt.thousandeyes.com |grep -v thousandeyes.com >> known_data_ip
+sort -u known_c1_ip | while read address;
+do
+iptables -A OUTPUT -d $addresses -p tcp --dport 443 -j DROP
+done
