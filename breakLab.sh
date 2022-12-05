68f1acae042d1c3a5a2c5ae44210fff4a20e1951
Soprek <tsoprek@gmai.com>
2022-12-05 09:26:31 +0100

    adding more sleep for DNS and setting up iptables to drop first as they are failing due to DNS lookup

--- breakLab.sh
+++ breakLab.sh
@@ -1,12 +1,13 @@
-./breakNTP.sh
-./breakID.sh
-./breakTeServ.sh
-./breakCAcert.sh
-./breakTestSSL.sh
 ./registryDrop.sh
 ./c1Drop.sh
 ./dataDrop.sh
+./breakID.sh
+sllep 1
+./breakTeServ.sh
+./breakCAcert.sh
+./breakTestSSL.sh
 sleep 10
+./breakNTP.sh
 ./breakDNS.sh
 > /var/log/te-agent.log
 
