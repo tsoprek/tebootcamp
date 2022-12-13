62007c96911478fdc7e55c58a92459c99d42ed45
Soprek <tsoprek@gmai.com>
2022-12-13 10:55:04 +0100

    validation task3 work on

--- task3Validation.sh
+++ task3Validation.sh
@@ -10,7 +10,7 @@ elif ! grep '10.48.26.73' /etc/netplan/00-installer-config.yaml 1>/dev/null && g
     if dig cisco.com; then
 	  echo '2'
 	  fi
-elif grep '10.48.26.74' /etc/netplan/00-installer-config.yaml 1>/dev/null && ! && grep '10.48.26.73' /etc/resolv.conf 1>/dev/null;
+elif grep '10.48.26.74' /etc/netplan/00-installer-config.yaml 1>/dev/null && ! grep '10.48.26.73' /etc/resolv.conf 1>/dev/null;
   then
 	echo '1'
 fi
