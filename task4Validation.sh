6424fbee25e4aa3583c20f44dce653012018d3fe
Soprek <tsoprek@gmai.com>
2022-12-14 14:26:45 +0100

    validation task4 work on

--- task4Validation.sh
+++ task4Validation.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+function eval() {
 sort -u known_registry_ip | while read address;
 do
     if iptables -S | grep $address 1>/dev/null;
 	then
- 	echo '1'
+ 	retval='1'
+	echo $retval
+     break 2
     fi
 done
-echo '0'
+}
+retval=$(eval)
+if [ "$retval" == "1" ] ; then
+	echo $retval
+else
+	echo '0'
+fi
