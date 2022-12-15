e116fdcb8b89785ae9d4ae1be8295f2e7dd35bd7
Soprek <tsoprek@gmai.com>
2022-12-15 11:49:12 +0100

    bug fix

--- breakTestSSL.sh
+++ breakTestSSL.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-if ! grep '#mozilla/GTS_Root_R1.crt' /etc/ca-certificates.conf;
-  then
   sed -i 's/^mozilla\/GTS_Root_R1.crt/#mozilla\/GTS_Root_R1.crt/g' /etc/ca-certificates.conf
   rm /usr/share/ca-certificates/mozilla/GTS_Root_R1.crt
   update-ca-certificates
   systemctl restart te-agent
-fi
+
--- fixTestSSL.sh
+++ fixTestSSL.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-if grep '#mozilla/GTS_Root_R1.crt' /etc/ca-certificates.conf;
-  then
   sed -i 's/^#mozilla\/GTS_Root_R1/mozilla\/GTS_Root_R1/g' /etc/ca-certificates.conf
   cp /root/.bootcampLab/GTS_Root_R1.crt /usr/share/ca-certificates/mozilla/
   update-ca-certificates
   systemctl restart te-agent
-fi
+
 
