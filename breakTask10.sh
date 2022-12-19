3a714d974448ad2f24827fb4321954e75190407c
Soprek <tsoprek@gmai.com>
2022-12-19 09:59:13 +0100

    Task 10 Validation Removed GPG key from task 10 as it never worked

--- breakTask10.sh
+++ breakTask10.sh
@@ -15,12 +15,3 @@ if ls /etc/apt/sources.list.d/thousandeyes.list~;
 	then
 	mv /etc/apt/sources.list.d/thousandeyes.list~ /etc/apt/sources.list.d/thousandeyes.list
 fi
-if ls /etc/apt/trusted.gpg~;
-	then
-	mv /etc/apt/trusted.gpg~ /etc/apt/trusted.gpg
-fi
-if gpg --show-keys /etc/apt/trusted.gpg | grep 'AA5FBA03CE4EF309B7E3D4E1C99A15F5BE7';
-	then
-	apt-key del AA5FBA03CE4EF309B7E3D4E1C99A15F5BE718900
-	echo 'GPG key removed!'
-fi
--- fixTask10.sh
+++ fixTask10.sh
@@ -11,31 +11,15 @@ else
 	echo 'Value is already set to 0'
 fi
 
-
 if ls /etc/apt/sources.list.d/thousandeyes.list~;
         then
         mv /etc/apt/sources.list.d/thousandeyes.list~ /etc/apt/sources.list.d/thousandeyes.list
-fi
-
-if ls /etc/apt/trusted.gpg~;
-        then
-        mv /etc/apt/trusted.gpg~ /etc/apt/trusted.gpg
 fi
 
 echo 'deb https://apt.thousandeyes.com focal main' > /etc/apt/sources.list.d/thousandeyes.list
 echo 'TE repo list added'
 
-if ! gpg --show-keys /etc/apt/trusted.gpg | grep 'AA5FBA03CE4EF309B7E3D4E1C99A15F5BE718900';
-then
-	apt-key add /etc/apt/trusted.gpg.d/thousandeyes-apt-key.pub.gpg
-	echo 'GPG key added!'
-fi
 if ls /etc/apt/sources.list.d/thousandeyes.list~;
         then
         mv /etc/apt/sources.list.d/thousandeyes.list~ /etc/apt/sources.list.d/thousandeyes.list
-fi
-
-if ls /etc/apt/trusted.gpg~;
-        then
-        mv /etc/apt/trusted.gpg~ /etc/apt/trusted.gpg
 fi
