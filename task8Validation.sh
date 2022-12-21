75bd343f4bbe297e1f56414533fc080d82a30637
te-tsoprek <tsoprek@thousandeyes.com>
2022-12-21 10:11:08 +0100

    Validation 8 bug

--- task8Validation.sh
+++ task8Validation.sh
@@ -6,7 +6,7 @@ do
   done | grep 'GTS Root R1' | wc -l
 }
 reteval=$(eval)
-if [ $reteval == 1 ] && grep '^mozilla/GTS_Root_R1' /etc/ca-certificates.conf 1>/dev/null;
+if [ $reteval -ge 1 ] && grep '^mozilla/GTS_Root_R1' /etc/ca-certificates.conf 1>/dev/null;
 then
 	echo '0'
 else
