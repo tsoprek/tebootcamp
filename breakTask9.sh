c36852f948a7ef70fa082602c5c58e9a6f3e4dc9
te-tsoprek <tsoprek@thousandeyes.com>
2023-08-23 13:28:35 +0200

    Fixed bug with task 9 where hardcoded time was 1 year ago and certificates were not valid due to time validation. This was too difficult for bootcamp.

--- breakTask9.sh
+++ breakTask9.sh
@@ -5,5 +5,6 @@
 #Exercise is to detect incorrect NTP servers, and work with ntp commands other then cut /etc/ntp.conf
 source lab_config
 sed -i "s/^server $ntp_server/#server $ntp_server/g" /etc/ntp.conf
-date --set="2022-11-30 10:05:59.990"
+date_hour_ago=`date -d '1 hour ago' "+%Y-%m-%d %H:%M:%S"`
+date --set="$date_hour_ago"
 systemctl restart ntp
