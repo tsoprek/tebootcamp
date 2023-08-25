bd85acf982b7cb61534451c65436e3d6bd212bf0
Ubuntu <tsoprek@gmail.com>
2023-08-25 09:25:53 +0000

    Changes on configure_lab

--- configure_lab.sh
+++ configure_lab.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-apt install python3-pip
-pip install flask
-iptables -A INPUT -p tcp --dport 5000 -j ACCEPT
-iptables -F
-cp 50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml
+sudo apt update
+sudo apt install -y python3-pip
+sudo pip install flask_sqlalchemy
+sudo iptables -A INPUT -p tcp --dport 5000 -j ACCEPT
+sudo iptables -F
+sudo cp 50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml
 openssl req -x509 -sha256 -days 1825 -newkey rsa:2048 -keyout te-bootcamp-key.pem -out te-bootcamp.pem
