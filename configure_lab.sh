ee1bb976351699e819764a627049161f8ad0441c
te-tsoprek <tsoprek@thousandeyes.com>
2023-08-25 12:57:30 +0200

    Testing new DNS and NTP

--- configure_lab.sh
+++ configure_lab.sh
@@ -4,6 +4,7 @@ sudo apt install -y python3-pip
 sudo pip install flask_sqlalchemy
 sudo iptables -A INPUT -p tcp --dport 5000 -j ACCEPT
 sudo iptables -F
+sudo apt install ntp
 curl -Os https://downloads.thousandeyes.com/agent/install_thousandeyes.sh
 sudo chmod +x install_thousandeyes.sh
 sudo ./install_thousandeyes.sh -b -l /var/log k4qcugs8yvi8bmhulm9fflz4al0kt138
