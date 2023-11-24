f191663b5fda3a43bd8c10644e339e9d4263fb30
te-tsoprek <tsoprek@thousandeyes.com>
2023-11-24 14:46:12 +0100

    Changed apt install paths which were HC and replaced with install_dir

--- configure_lab.sh
+++ configure_lab.sh
@@ -4,10 +4,8 @@ source lab_config
 #Run apt update for the first time to create metadata
 sudo apt update
 #Install Python-pip, Flask, NTP
-sudo apt install -y python3-pip
+sudo apt install -y python3-pip ntp sqlite3
 sudo pip install flask_sqlalchemy
-sudo apt install -y ntp
-sudo apt install sqlite3
 #Install TE agent
 curl -Os https://downloads.thousandeyes.com/agent/install_thousandeyes.sh
 sudo chmod +x install_thousandeyes.sh
@@ -16,12 +14,11 @@ sudo ./install_thousandeyes.sh -b -l /var/log k4qcugs8yvi8bmhulm9fflz4al0kt138
 sudo cp 50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml
 sudo netplan apply
 sudo dhclient
-
 #Allow tcp port 5000 in for flask and block 8.8.8.8 pushed from dhcp. We use DHCP from lab_config file
 sudo iptables -A INPUT -p tcp --dport 5000 -j ACCEPT
 sudo iptables -A OUTPUT -d 8.8.8.8 -p udp --dport 53 -j DROP
 #Generate certificate that is required for flask
-openssl req -x509 -sha256 -days 1825 -newkey rsa:2048 -keyout te-bootcamp-key.pem -out te-bootcamp.pem
+openssl req -x509 -nodes -sha256 -days 1825 -newkey rsa:2048 -keyout te-bootcamp-key.pem -out te-bootcamp.pem
 if ! grep install_dir lab_config;
   then
     echo 'install_dir='`pwd` >>lab_config
