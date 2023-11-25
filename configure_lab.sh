3870541be204690a79138d315d26389040d616c8
te-tsoprek <tsoprek@thousandeyes.com>
2023-11-25 11:27:49 +0100

    Changes to add tetraining user, set password, and add to wheel

--- configure_lab.sh
+++ configure_lab.sh
@@ -12,11 +12,18 @@ sudo chmod +x install_thousandeyes.sh
 sudo ./install_thousandeyes.sh -b -l /var/log k4qcugs8yvi8bmhulm9fflz4al0kt138
 #Replace network config file, apply config file and refresh DHCP
 sudo cp 50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml
+#Change of MAC address to in yaml file for match mac address
 macaddr=`ip a show dev ens2 scope link | grep link`
 sudo sed -i "s/52:54:00:13:85:fd/${macaddr:15:18}/g" /etc/netplan/50-cloud-init.yaml
+#Change of default DNS server to configured DNS server
 sudo sed -i "s/8.8.8.8/$dns_server/g" /etc/netplan/50-cloud-init.yaml
+#Apply config and refresh
 sudo netplan apply
 sudo dhclient
+#add tetraining user, set password, and add to wheel
+sudo useradd tetraining
+echo -e 'Krakow123\nKrakow123\n' | sudo passwd tetraining
+sudo usermod –aG wheel tetraining
 #Allow tcp port 5000 in for flask and block 8.8.8.8 pushed from dhcp. We use DHCP from lab_config file
 sudo iptables -A INPUT -p tcp --dport 5000 -j ACCEPT
 sudo iptables -A OUTPUT -d 8.8.8.8 -p udp --dport 53 -j DROP
@@ -26,5 +33,5 @@ if ! grep install_dir lab_config;
   then
     echo 'install_dir='`pwd` >>lab_config
   else
-    echo '*** install_dir already set to $install_dir VALIDATE MANUAL ***'
+    echo "*** install_dir already set to $install_dir VALIDATE MANUALLY ***"
 fi
