716c02a3898cde9e889c44bfbba96c3796fd4bff
te-tsoprek <tsoprek@thousandeyes.com>
2024-04-08 16:33:48 +0200

    Due to CML resolving DNS requests adding specific server permitted, all others blocked

--- task3Validation.sh
+++ task3Validation.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 source lab_config
 if grep "$dns_server" /etc/netplan/50-cloud-init.yaml 1>/dev/null;
-  then
-    if dig cisco.com 1>/dev/null; then
-	  echo '0'
-	  fi
 elif ! grep "$dns_server" /etc/netplan/50-cloud-init.yaml 1>/dev/null && grep "$dns_server" /etc/resolv.conf 1>/dev/null;
   then
     if dig cisco.com 1>/dev/null; then
