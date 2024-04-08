0dfe35ed2e2f25f221cc9c3c401beba089c75be7
te-tsoprek <tsoprek@thousandeyes.com>
2024-04-08 17:27:59 +0200

    Remove all default pools in NTP conf

--- breakTask9.sh
+++ breakTask9.sh
@@ -5,6 +5,7 @@
 #Exercise is to detect incorrect NTP servers, and work with ntp commands other then cut /etc/ntp.conf
 source lab_config
 sed -i "s/^server $ntp_server/#server $ntp_server/g" /etc/ntp.conf
+sed -i "s/^pool/#pool/g" /etc/ntp.conf
 date_hour_ago=`date -d '5.5 hour ago' "+%Y-%m-%d %H:%M:%S"`
 date --set="$date_hour_ago"
 systemctl restart ntp
