c9f9d20441b69d6be9f83fc2f1ebe375c81be336
te-tsoprek <tsoprek@thousandeyes.com>
2024-04-09 16:40:20 +0200

    x

--- task10Validation.sh
+++ task10Validation.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if grep 'auto-updates=1' /etc/te-agent.cfg 1>/dev/null && \
-  grep 'deb https://apt.thousandeyes.com focal main' /etc/apt/sources.list.d/thousandeyes.list 1>/dev/null; || \
-  grep 'deb https://apt.thousandeyes.com focal main' /etc/apt/sources.list;
+  grep 'deb https://apt.thousandeyes.com focal main' /etc/apt/sources.list.d/thousandeyes.list 1>/dev/null;
   then echo '0'
-
+elif grep 'auto-updates=1' /etc/te-agent.cfg 1>/dev/null && \
+  grep 'deb https://apt.thousandeyes.com focal main' /etc/apt/sources.list 1>/dev/null;
+  then echo '0'
 elif grep 'auto-updates=1' /etc/te-agent.cfg 1>/dev/null || \
   grep 'deb https://apt.thousandeyes.com focal main' /etc/apt/sources.list.d/thousandeyes.list 1>/dev/null;
   then echo '2'
-
 else
   echo '1'
 fi
