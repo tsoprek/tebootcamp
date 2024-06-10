77963fb7d20e857ef77e861546fc0a1a813a618c
te-tsoprek <tsoprek@thousandeyes.com>
2024-06-10 14:12:57 +0200

    while loop for iptables - DNS

--- fixTask3.sh
+++ fixTask3.sh
@@ -2,9 +2,9 @@
 source lab_config
 sed -i "s/$wrong_dns_server/$dns_server/g" /etc/netplan/50-cloud-init.yaml
 netplan apply
-if iptables -S | grep "udp --sport 53 -j DROP";
-  then sudo iptables -D INPUT -p udp --sport 53 -j DROP
-fi
+while iptables -S | grep "udp --sport 53 -j DROP";
+  do sudo iptables -D INPUT -p udp --sport 53 -j DROP
+done
 if grep "$dns_server" /etc/netplan/50-cloud-init.yaml > /dev/null ;
   then
   echo 'DNS Fixed'
