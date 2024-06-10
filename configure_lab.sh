0208cdec76f5df1e34d8b9657ee3b2ba738f9e4e
te-tsoprek <tsoprek@thousandeyes.com>
2024-06-10 12:47:51 +0200

    New DNS servers added. Adjusted IP tables

--- configure_lab.sh
+++ configure_lab.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 #call lab_config source to import variables
 source lab_config
+
+#Add TE repo
+echo 'deb https://apt.thousandeyes.com focal main' > /etc/apt/sources.list.d/thousandeyes.list
+sudo apt-add-repository https://apt.thousandeyes.com
+sudo wget -q https://apt.thousandeyes.com/thousandeyes-apt-key.pub -O- | sudo apt-key add -
+
 #Install TE agent
 apt install $install_dir/te-agent_1.147.0-1~focal_amd64.deb -y
 systemctl enable te-agent
 systemctl start te-agent
+
 #Run apt update for the first time to create metadata
 sudo apt update
+
 #Install Python-pip, Flask, NTP
 sudo apt install -y python3-pip ntp sqlite3 traceroute te-agent-utils snmp
-sudo pip install flask_sqlalchemy
+sudo pip install flask flask_sqlalchemy
+
 #Replace network config file, apply config file and refresh DHCP
 sudo cp 50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml
+
 #Change of MAC address to in yaml file for match mac address
 macaddr=`ip a show dev ens2 scope link | grep link`
 sudo sed -i "s/52:54:00:13:85:fd/${macaddr:15:18}/g" /etc/netplan/50-cloud-init.yaml
+
 #Change of default DNS server to configured DNS server
 sudo sed -i "s/8.8.8.8/$dns_server/g" /etc/netplan/50-cloud-init.yaml
-#Addin block for UDP port 53 default gateway as CML is resolving DNS requests
+#Addin block for UDP port 53 default gateway as CML is resolving DNS requests!! >> This is commented out as block DNS port53
+# is added with only permitting configured DNS server on port 53
 #dg_raw=`ip route show default`
 #dg=$(echo $dg_raw| cut -d' ' -f3)
 #if ! iptables -S | grep $dg;
@@ -26,20 +38,24 @@ sudo sed -i "s/8.8.8.8/$dns_server/g" /etc/netplan/50-cloud-init.yaml
 #  sudo iptables -A INPUT -s $dns_server -p tcp --sport 53 -j ACCEPT
 #  sudo iptables -A INPUT -p udp --sport 53 -j DROP
 #fi
+
 #Apply config and refresh
 sudo netplan apply
 sudo dhclient
+
 #add tetraining user/home/bash, set password, and add to wheel
 sudo useradd -m -s /bin/bash tetraining
 sudo chown -R tetraining:tetraining /home/tetraining
 sudo chmod 700 /home/tetraining
 echo -e 'Krakow123\nKrakow123\n' | sudo passwd tetraining
 sudo usermod -aG sudo tetraining
+
 #Allow tcp port 5000 in for flask
 if ! iptables -S | grep 5000;
   then
   sudo iptables -A INPUT -p tcp --dport 5000 -j ACCEPT
 fi
+
 #Generate certificate that is required for flask
 openssl req -x509 -nodes -sha256 -days 1825 -newkey rsa:2048 -keyout te-bootcamp-key.pem -out te-bootcamp.pem -subj "/CN=te-bootcamp.com"
 if ! grep install_dir lab_config;
@@ -48,4 +64,3 @@ if ! grep install_dir lab_config;
   else
     echo "*** install_dir already set to $install_dir VALIDATE MANUALLY ***"
 fi
-./runlab.sh
--- fixTask3.sh
+++ fixTask3.sh
@@ -2,7 +2,7 @@
 source lab_config
 sed -i "s/$wrong_dns_server/$dns_server/g" /etc/netplan/50-cloud-init.yaml
 netplan apply
-if ! iptables -S | grep "udp --sport 53 -j DROP";
+if iptables -S | grep "udp --sport 53 -j DROP";
   then sudo iptables -D INPUT -p udp --sport 53 -j DROP
 fi
 if grep "$dns_server" /etc/netplan/50-cloud-init.yaml > /dev/null ;
