2f7e0fd971cc0821f148decb91eefc15a92d8527
te-tsoprek <tsoprek@thousandeyes.com>
2024-06-14 11:25:25 +0200

    while loop for iptables - DNS

--- task8Validation.sh
+++ task8Validation.sh
@@ -3,7 +3,7 @@ function eval() {
 ls /usr/share/ca-certificates/mozilla | grep -i GlobalSign | while read  certificate;
 do
   openssl x509 -text -in /usr/share/ca-certificates/mozilla/$certificate;
-  done | grep 'GlobalSign' | wc -l
+  done | grep 'CN = GlobalSign Root CA' | wc -l
 }
 reteval=$(eval)
 if [ $reteval -ge 1 ] && grep '^mozilla/GlobalSign_Root_CA.crt' /etc/ca-certificates.conf 1>/dev/null;
